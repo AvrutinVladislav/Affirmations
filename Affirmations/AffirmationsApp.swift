f357429c44ade34b4ba6758a2b7e03c4e12f801e
Avrutin Vlad <avrutin1991@gmail.com>
2024-07-31 09:43:51 +0300

    start project

---
+++ Affirmations/AffirmationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AffirmationsApp.swift
+//  Affirmations
+//
+//  Created by Vladislav Avrutin on 31.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct AffirmationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
